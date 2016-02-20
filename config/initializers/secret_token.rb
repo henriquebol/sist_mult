2316a36922e90f05f36534272244ba803462a225
henriquebol <henbolima@gmail.com>
2016-02-20 00:22:27 -0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistMult::Application.config.secret_token = 'f31e5ca9abff2328e944061c8784051c1c978179e55dbb48e9a3c751f88f69616deb246ab9201d3411c157a220afac12bde7f4c3affe3a29f00920e9aef1137b'
