8161c77e725fcdcd721e75607fcc2aae9e839a3f
henriquebol <henbolima@gmail.com>
2016-02-20 17:28:04 -0300

    huffman 2

--- app/controllers/multimidia_controller.rb
+++ app/controllers/multimidia_controller.rb
@@ -6,17 +6,18 @@ class MultimidiaController < ApplicationController
     input = params[:input]
     resposta = Huffman.new(input)
 
-    puts $frequencies.length
+    base = (Math.sqrt($frequencies.length)).round
 
     bits_asc2 = input.length * 8.0
-    bits_3 = input.length * 3.0
+    bits_base = input.length * base
 
-    porcentagem_3 = bits_3/bits_asc2*100-100
+    porcentagem_base = bits_base/bits_asc2*100-100
     porcentagem_resposta = resposta.to_s.length/bits_asc2*100-100
 
     json = {  bits_asc2: bits_asc2, 
-              bits_3: bits_3,  
-              porcentagem_3: porcentagem_3.to_s + "%", 
+              base: base,
+              bits_base: bits_base,  
+              porcentagem_base: porcentagem_base.to_s + "%", 
               tamnaho_resposta: resposta.to_s.length,
               porcentagem_resposta: porcentagem_resposta.to_s + "%",
               resposta: resposta.to_s,
